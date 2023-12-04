61d046a414ac6d4e240125c33112f4dc99c0f463
Andrii <andrejsalogub3@gmail.com>
2023-12-04 15:38:27 +0200

    added dom content loader to prevent possible bugs

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-const board = document.querySelector(".game-board");
-const context = board.getContext("2d");
-const boardHeight = board.offsetHeight;
-const boardWidth = board.offsetWidth;
+document.addEventListener("DOMContentLoaded", () => {
+    const board = document.querySelector(".game-board");
+    const context = board.getContext("2d");
+    const boardHeight = board.offsetHeight;
+    const boardWidth = board.offsetWidth;
 
-const scoreText = document.querySelector("#score");
+    const scoreText = document.querySelector("#score");
 
-let running = false;
-let xVelocity = 25;
-let yVelocity = 0;
+    let running = false;
+    let xVelocity = 25;
+    let yVelocity = 0;
 
-let foodX;
-let foodY;
+    let foodX;
+    let foodY;
 
-let score = 0;
+    let score = 0;
 
-let snake = [
-    {
-        x: 0,
-        y: 0,
-    },
-];
+    let snake = [
+        {
+            x: 0,
+            y: 0,
+        },
+    ];
+});
