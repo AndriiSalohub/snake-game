756fb4b7c5a0b7fa15d05169de53392c88b4a81a
Andrii <andrejsalogub3@gmail.com>
2023-12-04 15:36:29 +0200

    added all the basic variables and got all the necessary elements

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const board = document.querySelector(".game-board");
+const context = board.getContext("2d");
+const boardHeight = board.offsetHeight;
+const boardWidth = board.offsetWidth;
+
+const scoreText = document.querySelector("#score");
+
+let running = false;
+let xVelocity = 25;
+let yVelocity = 0;
+
+let foodX;
+let foodY;
+
+let score = 0;
+
+let snake = [
+    {
+        x: 0,
+        y: 0,
+    },
+];
